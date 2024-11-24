116cf0d05f41b4d58423e05ca85df7dedd8444f6
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-24 10:56:19 +0000

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  get "/:username" => "users#show", as: :user
-
+  
   devise_for :users
   resources :user_tandas
   resources :transactions
   resources :tandas
   resources :analytics, only: [:index]
   
+  get "/:username" => "users#show", as: :user
 end
