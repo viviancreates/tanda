5608d23bd2d5b31d17fa8c7536f7f84fbab0fdab
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-24 10:53:34 +0000

    There is an issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:username])
-    
+
+
     if @user.nil?
       redirect_to root_path, alert: "User not found."
+    else
+      @tandas = @user.tandas
+      @transactions = @user.transactions
+
+      @connections = User.joins(:tandas)
+                         .where(tandas: { id: @tandas.pluck(:id) })
+                         .where.not(id: @user.id)
+                         .distinct
     end
-
-    @tandas = @user.tandas
-    @transactions = @user.transactions
-
-
-    @connections = User.joins(:tandas)
-                       .where(tandas: { id: @tandas.pluck(:id) })
-                       .where.not(id: @user.id)
-                       .distinct
   end
 end
