4cc8a8a75a00ed52a74f042131bda4ca0ef5f651
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-24 11:36:39 +0000

    Add analytics that are specific to user tandas

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,5 +1,15 @@
 class AnalyticsController < ApplicationController
   def index
-    @amount_per_user = Transaction.group(:user_tanda_id).sum(:amount)
+    @user_tandas = UserTanda.all
+    @charts_data = {}
+
+    @user_tandas.each do |user_tanda|
+      tanda = user_tanda.tanda
+      @charts_data[tanda.name] = tanda.user_tandas
+        .joins(:transactions) 
+        .joins(:user)
+        .group('users.username')
+        .pluck('users.username', 'SUM(transactions.amount)')
+    end
   end
 end
