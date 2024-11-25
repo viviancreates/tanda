272371eace852cf18151269acf187b8e13c8e0ba
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-25 00:59:11 +0000

    Add metrics to the tanda contrinutions chart

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -5,11 +5,22 @@ class AnalyticsController < ApplicationController
 
     @user_tandas.each do |user_tanda|
       tanda = user_tanda.tanda
-      @charts_data[tanda.name] = tanda.user_tandas
+      user_contributions = tanda.user_tandas
         .joins(:transactions) 
         .joins(:user)
         .group('users.username')
-        .pluck('users.username', 'SUM(transactions.amount)')
+        .sum('transactions.amount')
+
+      total_contributions = user_contributions.values.sum
+      goal_amount = tanda.goal_amount
+      remaining_goal = [goal_amount - total_contributions, 0].max
+
+      @charts_data[tanda.name] = {
+        user_data: user_contributions,
+        total: total_contributions,
+        goal: goal_amount,
+        remaining_goal: remaining_goal
+      }
     end
   end
 end
