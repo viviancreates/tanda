71734da2e46164b15abf51d5f657fb7965001168
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-28 07:49:52 +0000

    Coinbase API installed I think

--- Gemfile
+++ Gemfile
@@ -87,6 +87,8 @@ gem "simple_form"
 gem "carrierwave"
 gem 'chartkick'
 gem 'groupdate'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'coinbase-sdk'
 
 
 group :development do
---
+++ config/initializers/coinbase.rb
@@ -0,0 +1,11 @@
+# config/initializers/coinbase.rb
+
+api_key_name = ENV['COINBASE_API_KEY_NAME']
+api_key_private_key = ENV['COINBASE_API_KEY_PRIVATE_KEY']
+
+Coinbase.configure do |config|
+  config.api_key_name = api_key_name
+  config.api_key_private_key = api_key_private_key
+end
+
+Rails.logger.info("Coinbase SDK has been successfully configured.")
