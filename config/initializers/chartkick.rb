64628d8e4d1b06bc99be5e8798a36f2d32dfdeeb
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-28 00:19:28 +0000

    Add pastel colors to charts

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,26 @@
+# config/initializers/chartkick.rb
+Chartkick.options = {
+  colors: [
+    "#FFB3BA", # Soft Pink
+    "#FFDFBA", # Peach
+    "#FFFFBA", # Light Yellow
+    "#B9FBC2", # Mint Green
+    "#BAE1FF", # Baby Blue
+    "#AEC6CF", # Pastel Blue
+    "#D5AAFF", # Lavender
+    "#FFCCF9", # Light Pink
+    "#FFABAB", # Pale Red
+    "#FFC3A0", # Light Orange
+    "#D4A5A5", # Rose
+    "#F4BFBF", # Pale Salmon
+    "#D5E1DF", # Sea Foam Green
+    "#E2F0CB", # Pastel Green
+    "#A7BED3", # Dusty Blue
+    "#C6E2E9", # Soft Aqua
+    "#C2C2A3", # Muted Olive
+    "#EDEEC9", # Light Beige
+    "#FFF5BA", # Pale Yellow
+    "#F3D6E4", # Blush Pink
+    "#E4C1F9"  # Lilac
+  ]
+}
