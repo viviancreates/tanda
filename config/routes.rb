d9575139dc2c7a3bcd4b5a45ee2d3e85bebba02f
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-11-28 08:06:57 +0000

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,7 @@ Rails.application.routes.draw do
   resources :tandas
   resources :analytics, only: [:index]
   resources :follow_requests, only: [:update, :create, :destroy]
-  
-  resources :wallets, only: [:create, :show] do
-    member do
-      post :fund  
-      post :transfer
-    end
-  end
+  resources :wallets, only: [:show, :fund]
 
   get "/friends", to: "users#friends", as: :friends
   get "/search_users", to: "users#search", as: :search_users
