113a6685c6ae3ed7ee7c6efb1450763aae2898f0
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-02 22:31:17 +0000

    Save

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_29_084947) do
     t.datetime "updated_at", null: false
     t.string "default_address"
     t.decimal "balance", precision: 10, scale: 2, default: "0.0"
-    t.jsonb "wallet_data"
+    t.jsonb "wallet_data", default: {}
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
