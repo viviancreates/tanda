3ee708d7addb49087de60bbf806fe3c8ed3d4013
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-04 02:49:28 +0000

    Make tandas ransackable

--- app/models/tanda.rb
+++ app/models/tanda.rb
@@ -17,7 +17,7 @@ class Tanda < ApplicationRecord
   has_many :users, through: :user_tandas
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
-  def self.ransackable_associations(auth_object = nil)
+  def self.ransackable_attributes(auth_object = nil)
     ["due_date", "name"]
   end
   
