72eb3da01e43f0f7620775bc16175c59541e8cef
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-04 03:13:47 +0000

    Scaffold tasks

---
+++ db/migrate/20241204031318_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
