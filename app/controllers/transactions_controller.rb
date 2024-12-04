d9d0ce39a55a731983c9e621696d355fff962f12
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-04 01:41:44 +0000

    Pause

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,9 @@ class TransactionsController < ApplicationController
   
   # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.joins(user_tanda: :user).where(user_tandas: { user_id: current_user.id }).order(:date).page(params[:page]).per(5)
+    @q = Transaction.ransack(params[:q])
+
+    @transactions = @q.result(distinct: true).joins(user_tanda: :user).where(user_tandas: { user_id: current_user.id }).order(:date).page(params[:page]).per(5)
   end
 
   # GET /transactions/1 or /transactions/1.json
