9c4115ca9371125158e42c762640beea8ebec210
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-09 04:24:53 +0000

    Clean up format

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,26 +6,26 @@ class AnalyticsController < ApplicationController
     @user_total_contributions = current_user.transactions.sum(:amount)
 
     pastel_colors = [
-      "#FFB3BA", # Soft Pink
-      "#FFDFBA", # Peach
-      "#FFFFBA", # Light Yellow
-      "#B9FBC2", # Mint Green
-      "#BAE1FF", # Baby Blue
-      "#AEC6CF", # Pastel Blue
-      "#D5AAFF", # Lavender
-      "#FFCCF9", # Light Pink
-      "#FFABAB", # Pale Red
-      "#FFC3A0"  # Light Orange
+      "#FFB3BA",
+      "#FFDFBA",
+      "#FFFFBA",
+      "#B9FBC2",
+      "#BAE1FF",
+      "#AEC6CF",
+      "#D5AAFF",
+      "#FFCCF9",
+      "#FFABAB",
+      "#FFC3A0",
     ]
 
     @user_tandas.each do |user_tanda|
       tanda = user_tanda.tanda
       next if tanda.nil?
       user_contributions = tanda.user_tandas
-        .joins(:transactions) 
+        .joins(:transactions)
         .joins(:user)
-        .group('users.username')
-        .sum('transactions.amount')
+        .group("users.username")
+        .sum("transactions.amount")
 
       total_contributions = user_contributions.values.sum
       goal_amount = tanda.goal_amount
@@ -33,16 +33,14 @@ class AnalyticsController < ApplicationController
 
       chart_colors = pastel_colors.sample(pastel_colors.length)
 
-
       @charts_data[tanda.name] = {
         user_data: user_contributions,
         total: total_contributions,
         goal: goal_amount,
         remaining_goal: remaining_goal,
-        colors: chart_colors
+        colors: chart_colors,
       }
     end
     @user_contribution_line = current_user.transactions.group_by_day(:date).sum(:amount)
   end
-  
 end
