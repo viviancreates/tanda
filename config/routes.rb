64f6101ba0d85fd18108ea7afb247efae874dcf1
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-12-11 05:13:21 +0000

    Protect route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  authenticate :user, ->(user) { user.admin? } do
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  end
+
   root to: "home#index"
   
   devise_for :users
